4ba99fb015fee92826fefec39039be4ff68a05e6
p singh <117709864+P-singh07@users.noreply.github.com>
2022-11-08 11:50:21 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Dinner!!")
+
+streamlit.header("Breakfast Menu")
+streamlit.text("Omega 3 blueberry oat meal")
+streamlit.text("Kale, Spinach & Rocket Smoothie")
+strealit.text("Hard-Boiled free Range EGG")
