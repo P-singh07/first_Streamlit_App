8553b9ae62fd3c6ef0ffe48405fd9f9894e63f94
p s <117709864+P-singh07@users.noreply.github.com>
2022-12-17 01:26:15 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-import snowflake.connector
+#import Snowflake.connector
 from urllib.error import URLERROR
 
 streamlit.title("My Parents New Healthy Dinner!!")
